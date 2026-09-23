f8a23352f11d38853b94f179cb28e878fb2bf8f1
anishhh18 <anish990990@gmail.com>
2026-09-23 12:12:13 +0530

    Project setup

---
+++ 1/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello Sir, My name is ${person}`;
+}
+let username = "Anish";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ 1/src/index.ts
@@ -0,0 +1,7 @@
+function greet(person:string):string {
+  return `Hello Sir, My name is ${person}`
+}
+
+let username:string = "Anish"
+
+console.log(greet(username))
