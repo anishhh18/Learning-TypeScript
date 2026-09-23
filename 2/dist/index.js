88e466ace480893199e3be057b4975596678569e
anishhh18 <anish990990@gmail.com>
2026-09-23 12:27:21 +0530

    Type Inference vs. Annotation

---
+++ 2/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let drink = "chai";
+let cups = Math.random() > 0.5 ? 10 : "5";
+let channelName = "ChaiOrCode";
+let chaiFlavour = "masalaChai";
+chaiFlavour = "gingerChai";
+console.log(chaiFlavour);
+//# sourceMappingURL=index.js.map
---
+++ 2/src/index.ts
@@ -0,0 +1,13 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : "5"
+
+let channelName = "ChaiOrCode"
+
+let chaiFlavour:string = "masalaChai"
+chaiFlavour="gingerChai"
+console.log(chaiFlavour)
+
+let chaiOder:number=5
+
+let isChaiServed:boolean = false
